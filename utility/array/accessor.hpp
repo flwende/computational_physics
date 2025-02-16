1dd73578c9ac70c24522f68655bbd0b13c1edfca
Florian Wende <flwende@gmail.com>
2025-02-16 17:20:00 +0100

    Remove iostream from source files.

--- utility/array/accessor.hpp
+++ utility/array/accessor.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <algorithm>
 #include <array>
 #include <cstdint>
--- utility/array/multi_dimensional_array.hpp
+++ utility/array/multi_dimensional_array.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <array>
 #include <cstdint>
 #include <memory>
