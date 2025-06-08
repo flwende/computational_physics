948a22122d798f12aa8d711847b5d0e0bf50093c
Florian Wende <flwende@gmail.com>
2025-06-08 18:06:33 +0200

    Calculate 1d index from 3d block goemetry.

--- random/include/random/lcg32_amd_gpu.hpp
+++ random/include/random/lcg32_amd_gpu.hpp
@@ -9,42 +9,54 @@ std::uint32_t Get(const std::int32_t index) const
 __device__
 void Load(const LCG32_State<WaveFrontSize>& other)
 {
-    state[threadIdx.x] = other.state[threadIdx.x];
-    a[threadIdx.x] = other.a[threadIdx.x];
-    c[threadIdx.x] = other.c[threadIdx.x];
+    const std::int32_t index = threadIdx.z * blockDim.x * blockDim.y
+        + threadIdx.y * blockDim.x + threadIdx.x;
 
-    if (threadIdx.x == 0)
+    state[index] = other.state[index];
+    a[index] = other.a[index];
+    c[index] = other.c[index];
+
+    if (index == 0)
         iteration = other.iteration;
 }
 
 __device__
 void Unload(LCG32_State<WaveFrontSize>& other) const
 {
-    other.state[threadIdx.x] = state[threadIdx.x];
-    other.a[threadIdx.x] = a[threadIdx.x];
-    other.c[threadIdx.x] = c[threadIdx.x];
+    const std::int32_t index = threadIdx.z * blockDim.x * blockDim.y
+        + threadIdx.y * blockDim.x + threadIdx.x;
 
-    if (threadIdx.x == 0)
+    other.state[index] = state[index];
+    other.a[index] = a[index];
+    other.c[index] = c[index];
+
+    if (index == 0)
         other.iteration = iteration;
 }
 
 __device__
 void Shuffle()
 {
-    if (threadIdx.x == 0)
+    const std::int32_t index = threadIdx.z * blockDim.x * blockDim.y
+        + threadIdx.y * blockDim.x + threadIdx.x;
+
+    if (index == 0)
         iteration += shuffle_distance;
 
     constexpr std::int32_t m = WaveFrontSize - 1;
     const std::int32_t shuffle_val = state[iteration & m] + (iteration & 1 ? 0 : 1);
 
-    a[threadIdx.x] = a[(threadIdx.x + shuffle_val) & m];
-    c[threadIdx.x] = c[(threadIdx.x + shuffle_val) & m];
+    a[index] = a[(index + shuffle_val) & m];
+    c[index] = c[(index + shuffle_val) & m];
 }
 
 __device__
 void Update()
 {
-    state[threadIdx.x] = a[threadIdx.x] * state[threadIdx.x] + c[threadIdx.x];
+    const std::int32_t index = threadIdx.z * blockDim.x * blockDim.y
+        + threadIdx.y * blockDim.x + threadIdx.x;
+
+    state[index] = a[index] * state[index] + c[index];
 }
 
 __device__
