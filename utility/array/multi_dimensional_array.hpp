eac201166986cebeb69b080d7416ada7795e8b51
Florian Wende <flwende@gmail.com>
2025-09-18 16:02:34 +0200

    Change relation between MultiDimensionalArray and NonOwningMultiDimensionalArray from 'is-a' to 'has-a': use composition pattern instead of inheritance.

--- utility/array/multi_dimensional_array.hpp
+++ utility/array/multi_dimensional_array.hpp
@@ -18,44 +18,48 @@ namespace XXX_NAMESPACE
     {
         static_assert(Dimension > 0, "Error: Dimension must be larger than 0.");
 
+        template <typename, std::uint32_t>
+        friend class MultiDimensionalArray;
+
         protected:
             Accessor<T, Dimension> accessor;
 
         public:
-            explicit NonOwningMultiDimensionalArray(T* external_ptr, const std::array<std::int32_t, Dimension>& extent)
+            explicit NonOwningMultiDimensionalArray(T* external_ptr, const std::array<std::int32_t, Dimension>& extent) noexcept
                 :
                 accessor(external_ptr, extent)
             {}
 
-            auto Initialized() const { return accessor.RawPointer() != nullptr; }
-            const auto& Extent() const { return accessor.Extent(); }
-            auto TotalElements() const { return accessor.TotalElements(); }
+            auto Initialized() const noexcept { return accessor.RawPointer() != nullptr; }
+            const auto& Extent() const noexcept { return accessor.Extent(); }
+            auto TotalElements() const noexcept { return accessor.TotalElements(); }
 
-            T* RawPointer() { return accessor.RawPointer(); }
-            const T* RawPointer() const { return accessor.RawPointer(); }
+            T* RawPointer() noexcept { return accessor.RawPointer(); }
+            const T* RawPointer() const noexcept { return accessor.RawPointer(); }
 
-            inline auto operator[](const std::int32_t index) -> decltype(accessor[0]) { return accessor[index]; }
-            inline auto operator[](const std::int32_t index) const -> decltype(accessor[0]) { return accessor[index]; }
+            auto operator[](const std::int32_t index) -> decltype(accessor[0]) { return accessor[index]; }
+            auto operator[](const std::int32_t index) const -> decltype(accessor[0]) { return accessor[index]; }
 
         protected:
-            NonOwningMultiDimensionalArray() = default;
+            NonOwningMultiDimensionalArray() noexcept = default;
     };
 
     template <typename T, std::uint32_t Dimension>
-    class MultiDimensionalArray final : public NonOwningMultiDimensionalArray<T, Dimension>
+    class MultiDimensionalArray final
     {
-        using Base = NonOwningMultiDimensionalArray<T, Dimension>;
         using PointerType = std::unique_ptr<T[]>;
 
         private:
-            PointerType data;
+            PointerType data {};
+            NonOwningMultiDimensionalArray<T, Dimension> span {};
 
         public:
-            MultiDimensionalArray() = default;
+            MultiDimensionalArray() noexcept = default;
 
             explicit MultiDimensionalArray(const std::array<std::int32_t, Dimension>& extent)
                 :
-                MultiDimensionalArray(std::make_unique<T[]>(std::accumulate(std::begin(extent), std::end(extent), 1, std::multiplies<std::size_t>())), extent)
+                data(std::make_unique<T[]>(std::accumulate(std::begin(extent), std::end(extent), 1, std::multiplies<std::size_t>()))),
+                span(data.get(), extent)
             {}
             
             MultiDimensionalArray(const MultiDimensionalArray&) = delete;
@@ -63,6 +67,16 @@ namespace XXX_NAMESPACE
 
             MultiDimensionalArray(MultiDimensionalArray&&) noexcept = default;
             MultiDimensionalArray& operator=(MultiDimensionalArray&&) noexcept = default;
+
+            auto Initialized() const { return span.Initialized(); }
+            const auto& Extent() const noexcept { return span.Extent(); }
+            auto TotalElements() const noexcept { return span.TotalElements(); }
+
+            T* RawPointer() noexcept { return span.RawPointer(); }
+            const T* RawPointer() const noexcept { return span.RawPointer(); }
+
+            auto operator[](const std::int32_t index) -> decltype(span[0]) { return span[index]; }
+            auto operator[](const std::int32_t index) const -> decltype(span[0]) { return span[index]; }
 
             void Resize(const std::array<std::int32_t, Dimension>& extent)
             {
@@ -72,19 +86,9 @@ namespace XXX_NAMESPACE
 
             auto DeepCopy() const
             {
-                using Base::Extent;
-                using Base::TotalElements;
-                using Base::RawPointer;
-
-                MultiDimensionalArray tmp(Extent());
-                std::copy_n(std::begin(RawPointer()), TotalElements(), tmp.RawPointer());
+                MultiDimensionalArray tmp(span.Extent());
+                std::copy_n(RawPointer(), TotalElements(), tmp.RawPointer());
                 return tmp;
             }
-
-        protected:
-            MultiDimensionalArray(PointerType&& data, const std::array<std::int32_t, Dimension>& extent)
-                :
-                Base(data.get(), extent), data(std::move(data))
-            {};
     };
 }
